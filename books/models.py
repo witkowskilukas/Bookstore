ed840836838918d5fae4c8147aa25d5b6cf37e00
Lukas <witkowskilukasz86@gmail.com>
2025-02-07 16:13:37 +0100

    chp13

--- books/models.py
+++ books/models.py
@@ -14,6 +14,7 @@ class Book(models.Model):
     title = models.CharField(max_length=60)
     author = models.CharField(max_length=60)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    cover = models.ImageField(upload_to="covers/", blank=True)
 
     def __str__(self):
         return self.title
--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     # Django admin
@@ -27,4 +29,4 @@ urlpatterns = [
     path('', include('pages.urls')),
     path('books/', include('books.urls'))
  
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
