864ae51e283555a6133635d1983202328e23d192
Lukas <witkowskilukasz86@gmail.com>
2025-03-14 17:55:42 +0100

    Add production settings

--- config/settings.py
+++ config/settings.py
@@ -31,8 +31,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DJANGO_DEBUG")
-
+DEBUG = env.bool("DJANGO_DEBUG", default=False)
+ 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1','example.com']
 
 
@@ -208,3 +208,15 @@ MEDIA_ROOT = BASE_DIR / "media"
 import socket
 hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
 INTERNAL_IPS = [ip[:-1] + "1" for ip in ips]
+
+
+SECURE_SSL_REDIRECT = env.bool("DJANGO_SECURE_SSL_REDIRECT", default=True)
+
+#HSTS configuration
+SECURE_HSTS_SECONDS = env.int("DJANGO_SECURE_HSTS_SECONDS",default=2592000)
+SECURE_HSTS_INCLUDE_SUBDOMAINS = env.bool("DJANGO_SECURE_HSTS_INCLUDE_SUBDOMAINS",default=True)
+SECURE_HSTS_PRELOAD = env.bool("DJANGO_SECURE_HSTS_PRELOAD", default=True)
+
+# Cookiese conf/for production
+SESSION_COOKIE_SECURE = env.bool("DJANGO_SESSION_COOKIE_SECURE", default=True)
+CSRF_COOKIE_SECURE = env.bool("DJANGO_CSRF_COOKIE_SECURE", default=True)
--- config/urls.py
+++ config/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     # Django admin
-    path("admin/", admin.site.urls),
+    path("just_casual_admin/", admin.site.urls),
     # User management
     path("accounts/", include("allauth.urls")),
     # Local apps
